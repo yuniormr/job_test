c418ac0a911381acb66fc78e57e415ab04ff7393
yuniormr <yuniormr@gmail.com>
2023-06-29 15:51:59 -0400

    save 1

---
+++ src/app/components/ServicesCard/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+
+export interface IServiceCardProps {
+    id: string;
+    title: string;
+    description: string;
+    link: string;
+    icon: React.ReactNode;
+}
+export const ServiceCard = ({title, description, link, icon}: IServiceCardProps) => {
+    return (
+        <div className="h-[22rem] w-[17rem] text-[#0f0f0f] hover:text-white hover:bg-[#2E53A3] bg-white p-4 pt-6 shadow-2xl">
+            {icon}
+            <h4 className="font-sans text-xl font-semibold text-inherit mt-3">{title}</h4>
+            <p className="font-sans text-sm text-inherit mt-3">{description}</p>
+            <Link href={link} className="flex gap-1 items-center font-semibold text-inherit text-lg font-sans mt-3">
+                Leer más
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                     strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-inherit">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M8.25 4.5l7.5 7.5-7.5 7.5"/>
+                </svg>
+            </Link>
+        </div>
+    );
+}
